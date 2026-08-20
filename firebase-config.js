adafa56d65a90d34258ab476d713aa91130d2c4e
Federico <federico.ladogana@gmail.com>
2026-08-20 10:47:43 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 // Li trovi in: Console Firebase -> icona ingranaggio -> Impostazioni progetto
 // -> scorri fino a "Le tue app" -> app web -> "Configurazione SDK".
 export const firebaseConfig = {
-  apiKey: "AIzaSyDk•••••••••••••••••••••••••••••••",
+  apiKey: "AIzaSyCj_cjaGbKn-c8WS8mc4g0Uw1jYOOPPnwE",
   authDomain: "decabo-ser.firebaseapp.com",
   projectId: "decabo-ser",
   storageBucket: "decabo-ser.firebasestorage.app",
